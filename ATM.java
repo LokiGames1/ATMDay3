da114833638cd6b94ca7633f02900ead5392adc1
Asher Engelberg <ashercengelberg@icloud.com>
2023-09-01 10:43:49 -0700

    Added Deposit and Withdraw methods
    added the functions, still need to fiddle with end cases and try catch

--- ATM.java
+++ ATM.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.io.IOException;
 
 public class ATM {
     static HashMap<String, Double> accounts = new HashMap<String, Double>();
@@ -8,11 +9,15 @@ public class ATM {
     }
 
     public static void openAccount(String userID, double initAmt) {
-        if (accounts.containsKey(userID)) {
-            throw new Error("Oops! There's already an account under that name. Please try a different one");
-        } else {
-            accounts.put(userID, initAmt);
+        try {
+            if (accounts.containsKey(userID)) {
+                throw new IOException();
+            } else {
+                accounts.put(userID, initAmt);
+            }
+        } catch (Exception e) {
         }
+
     }
 
     public static void closeAccount(String userID) {
@@ -28,4 +33,37 @@ public class ATM {
         }
     }
 
+    public static double checkBalance(String userID) {
+        if (!accounts.containsKey(userID)) {
+            throw new Error("There isn't an account associated with that username, dumba**. Sorry!");
+        } else {
+            return accounts.get(userID);
+        }
+    }
+
+    public static double depositMoney(String userID, double amount) {
+        if (!accounts.containsKey(userID)) {
+            throw new Error("There isn't an account associated with that username. You're broke AF");
+        } else {
+            double currentMoney = accounts.get(userID);
+            accounts.put(userID, amount + currentMoney);
+            return amount;
+        }
+
+    }
+
+    public static double withdrawMoney(String userID, double amount) {
+        if (!accounts.containsKey(userID)) {
+            throw new Error("There isn't an account associated with that username. You're broke AF");
+        } else {
+            if (accounts.get(userID) == 0) {
+                throw new Error("Sorry bro, you're broke");
+            }
+            double currentMoney = accounts.get(userID);
+            double newMoney = currentMoney - amount;
+
+            accounts.put(userID, newMoney);
+            return amount;
+        }
+    }
 }
